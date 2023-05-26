10441997e4c82ddfc9c54c39d0dd78c37d02f1ed
RuanGeldenhuys <ruangeldenhuys.work@gmail.com>
2023-05-26 13:56:00 +0200

    Update Modeling.R
    DId a rough hyperparameter tune and calculated variable importance

--- code/Modeling.R
+++ code/Modeling.R
@@ -1,10 +1,12 @@
 #Modeling
+library(tictoc)
 set.seed(246)
 indices <- createDataPartition(finalDF$UpgradeInternet, p = 0.8, list = FALSE)
 
 train_set <- finalDF[indices,]
 test_set <- finalDF[-indices,]
 n_features <- ncol(finalDF)-1
+tic()
 rf1 <- ranger(
     UpgradeInternet ~ .,
     data = train_set,
@@ -15,18 +17,19 @@ rf1 <- ranger(
     num.trees = n_features * 10
 )
 rf1
+toc()
 
 #Hyper parameter tuning
 hyper_grid <- expand.grid(
-    mtry = floor(n_features * c(.05, .15, .25, .333, .4)),
-    min.node.size = c(1, 3, 5, 10),
-    replace = c(TRUE, FALSE),
-    sample.fraction = c(.5, .63, .8),
-    num.trees = n_features* c(5,10,15,20),
-    predError = NA,
-    rmse = NA
+    mtry            = floor(n_features * c(.05, .10, .15, .20, .25, .30)),
+    min.node.size   = c(1, 3, 5, 10),
+    replace         = c(TRUE, FALSE),
+    sample.fraction = c(.5, .63, .8, 1),
+    num.trees       = n_features* c(5,10,15,20),
+    predError       = NA,
+    rmse            = NA
 )
-
+tic()
 for(i in seq_len(nrow(hyper_grid))) {
     # fit model for ith hyperparameter combination
     fit <- ranger(
@@ -38,14 +41,33 @@ for(i in seq_len(nrow(hyper_grid))) {
         replace         = hyper_grid$replace[i],
         sample.fraction = hyper_grid$sample.fraction[i],
         verbose         = FALSE,
-        seed            = 123,
+        seed            = 246,
         respect.unordered.factors = 'order',
     )
     # export OOB error
-    hyper_grid$predError <- fit$prediction.error
+    hyper_grid$predError[i] <- fit$prediction.error
     hyper_grid$rmse[i] <- sqrt(fit$prediction.error)
 }
+toc()
 
 hyper_grid %>%
     arrange(rmse) %>%
     head(10)
+
+bestmod1 <- ranger(
+    UpgradeInternet ~ .,
+    data = train_set,
+    mtry = 4,
+    min.node.size = 10,
+    replace = FALSE,
+    sample.fraction = 0.8,
+    num.trees = 440,
+    seed = 246,
+    importance = 'impurity',
+    respect.unordered.factors = 'order'
+)
+bestmod1
+VarImp <- importance(bestmod1)
+VarImp <- sort(VarImp, decreasing = TRUE)
+VarImp10 <- head(VarImp,10)
+plot(VarImp10)
